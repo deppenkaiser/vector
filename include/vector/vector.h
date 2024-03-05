6e3b5ae5cc8e2f2df9669d81dcb6480d7cd43be1
Dipl.-Ing. (FH) Michael Czybor <contact@log1cworx.at>
2024-03-05 10:49:20 +0100

    CHG new functions

--- include/vector/vector.h
+++ include/vector/vector.h
@@ -1,2 +1,11 @@
 #pragma once
 
+typedef struct vector_3d
+{
+    double x;
+    double y;
+    double z;
+} *vector_3d_t;
+
+struct vector_3d vector_sub(vector_3d_t v1, vector_3d_t v2);
+double vector_norm(vector_3d_t v);
--- src/vector.c
+++ src/vector.c
@@ -1,2 +1,16 @@
 #include "vector/vector.h"
+#include <math.h>
 
+struct vector_3d vector_sub(vector_3d_t v1, vector_3d_t v2)
+{
+    struct vector_3d v = {0};
+    v.x = v1->x - v2->x;
+    v.y = v1->y - v2->y;
+    v.z = v1->z - v2->z;
+    return v;
+}
+
+double vector_norm(vector_3d_t v)
+{
+    return sqrt(v->x * v->x + v->y * v->y + v->z * v->z);
+}
