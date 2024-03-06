f245d4d807eda1d00e88567de718aa35ecbb1570
Dipl.-Ing. (FH) Michael Czybor <contact@log1cworx.at>
2024-03-06 17:12:55 +0100

    ADD new functions

--- include/vector/vector.h
+++ include/vector/vector.h
@@ -7,5 +7,7 @@ typedef struct vector_3d
     double z;
 } *vector_3d_t;
 
+struct vector_3d vector_add(vector_3d_t v1, vector_3d_t v2);
 struct vector_3d vector_sub(vector_3d_t v1, vector_3d_t v2);
 double vector_norm(vector_3d_t v);
+void vector_clear(vector_3d_t v);
--- src/vector.c
+++ src/vector.c
@@ -1,6 +1,15 @@
 #include "vector/vector.h"
 #include <math.h>
 
+struct vector_3d vector_add(vector_3d_t v1, vector_3d_t v2)
+{
+    struct vector_3d v = {0};
+    v.x = v1->x + v2->x;
+    v.y = v1->y + v2->y;
+    v.z = v1->z + v2->z;
+    return v;
+}
+
 struct vector_3d vector_sub(vector_3d_t v1, vector_3d_t v2)
 {
     struct vector_3d v = {0};
@@ -13,4 +22,9 @@ struct vector_3d vector_sub(vector_3d_t v1, vector_3d_t v2)
 double vector_norm(vector_3d_t v)
 {
     return sqrt(v->x * v->x + v->y * v->y + v->z * v->z);
+}
+
+void vector_clear(vector_3d_t v)
+{
+    v->x = v->y = v->z = 0.0;
 }
